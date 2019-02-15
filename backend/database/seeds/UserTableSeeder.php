5a9ef47e3b6b001e98f2316bb319519401aeb4d3
Mrdai <346139312@qq.com>
2019-02-15 17:01:45 +0800

    update

--- backend/database/seeds/UserTableSeeder.php
+++ backend/database/seeds/UserTableSeeder.php
@@ -10,7 +10,7 @@ class UserTableSeeder extends Seeder {
      *
      * @return void
      */
-    public function run() {
+    public function run () {
         DB::table("user")->insert([
             "username"   => "root",
             "openid"     => "oWqQa6K2egw4ijKVOAC-tffxhxKf",
@@ -56,6 +56,35 @@ class UserTableSeeder extends Seeder {
             "openid"     => "ocYxcuAEy30bX0NXmGn4ypqx3tqq",
             "Identity"   => 0,
             "remarks"    => "user1_remarks",
+            "created_at" => date('Y-m-d h:i:s', time()),
+            "updated_at" => date('Y-m-d h:i:s', time()),
+        ]);
+        DB::table("user")->insert([
+            "username"     => "亮",
+            "head_img_url" => "http://thirdwx.qlogo.cn/mmopen/PiajxSqBRaEIgje8a8iaqO7GIzSAPwXg5uEISQbBT0TXBRCqyaBO9JHrehl9lPgErDicibB2gLicFXJib05vAbs8nU0w/132",
+            "openid"       => "oWqQa6FfShBlltzOg3QQ8SXzOYRk",
+            "Identity"     => 0,
+            "remarks"      => "",
+            "created_at"   => date('Y-m-d h:i:s', time()),
+            "updated_at"   => date('Y-m-d h:i:s', time()),
+        ]);
+        DB::table("user")->insert([
+            "username"     => "墨渊上神",
+            "real_name"    => "苏军华",
+            "phone"        => "17689975996",
+            "head_img_url" => "http://thirdwx.qlogo.cn/mmopen/4wIU4IwY5dibkyVsg99ahreibZYPFEtGYSPebV0VeI59JNcIiaB33bdYm0mGAObB4QyMz0PgJpZ7sFH4Iw2zsVZicicjZnyE8wttR/132",
+            "openid"       => "oWqQa6IyFXdosOx5uroFusiIUGz8",
+            "Identity"     => 0,
+            "remarks"      => "",
+            "created_at"   => date('Y-m-d h:i:s', time()),
+            "updated_at"   => date('Y-m-d h:i:s', time()),
+        ]);
+        DB::table("user")->insert([
+            "username"   => "五哥潮鞋 正常接单",
+            "head_img_url" => "http://thirdwx.qlogo.cn/mmopen/TxgpkT9ibpnHiah9foWlLkknjcJNLmOa19nqEgmnCsbicoyFYIGUZicHP5AVN0FbcIHyta8Tn3BSsMoXTWEsLQd3wgsmwa7mxdqib/132",
+            "openid"     => "oWqQa6HraV3D-4FXZe09Q7WZPidk",
+            "Identity"   => 0,
+            "remarks"    => "",
             "created_at" => date('Y-m-d h:i:s', time()),
             "updated_at" => date('Y-m-d h:i:s', time()),
         ]);
